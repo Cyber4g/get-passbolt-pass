466177ada60e69e9d5147de5cad5452e19af3420
Alex <47859571+Cyber4g@users.noreply.github.com>
2024-09-04 12:13:30 +0300

    First push

---
+++ passbolt_get_pass.py
@@ -0,0 +1,27 @@
+from passbolt import PassboltAPI
+import json
+
+# открывает конф файл
+with open("config.json") as config_file:
+    dict_config = json.load(config_file)
+
+p = PassboltAPI(dict_config=dict_config)
+
+name = input("enter name: ")
+# получение списка УЗ
+resource = next((item for item in p.get_resources() if item["username"] == name), None)
+
+# p.get_resource_secret - получение шифрованного пароля для найденной УЗ
+# дешифровка пароля с помощью p.decrypt() и преобразование дешифрованного пароля в JSON-формат.
+if resource is not None:
+    res = (
+            dict_config.get("gpg_library", "PGPy") == "gnupg"
+            and json.loads(p.decrypt(p.get_resource_secret(resource["id"])).data)
+            or json.loads(p.decrypt(p.get_resource_secret(resource["id"])))
+    )
+
+    print(res)
+    username = resource["username"]
+    password = res["password"]
+    print("Username: ", username)
+    print("Password:", password)
